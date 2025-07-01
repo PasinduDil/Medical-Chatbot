d635a83877afb074e26ca6847cd74bed82e510ef
PasinduDil <blackfoxxpasi@gmai.com>
2025-07-01 14:27:47 +0530

    update stracture

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical-Chatbot',
+    version= '0.0.1',
+    author= 'Pasindu Dilshan',
+    author_email= 'Pasindu2035@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
